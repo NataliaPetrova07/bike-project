a2ae59e6635fdbf1587c7d9e528c37e3086c5cbc
Natalia Petrova <petrova.ntl@gmail.com>
2022-03-26 20:07:12 +0100

    dynamic data attempt

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ function init(event) {
 
 async function loadBikes() {
   const response = await fetch(
-    "https://wps22.it-studerende.dk/wp-json/wp/v2/bike?_embed"
+    // "https://wps22.it-studerende.dk/wp-json/wp/v2/bike?_embed"
+    "https://nataliapetro.com/wp090102/wp-json/wp/v2/bike?_embed"
   );
   //   console.log("lacj-response", response);
   const thedata = await response.json();
@@ -26,8 +27,9 @@ function displayData(bikes) {
         "wp:featuredmedia"
       ][0].media_details.sizes.medium_large.source_url;
     /* console.log("catName", bike._embedded["wp:term"][0][1].name); */
-    cloneEl.querySelector("h3").textContent =
-      bike._embedded["wp:term"][0][1].name;
+    // cloneEl.querySelector("h3").textContent =
+    //   bike._embedded["wp:term"][0][1].name;
+    cloneEl.querySelector("h3").textContent = bike.categories.brand;
     cloneEl.querySelector("h2").textContent = bike.title.rendered;
     let priceEl = cloneEl.querySelector(".price span");
     let priceText = bike.price;
@@ -43,18 +45,18 @@ function displayData(bikes) {
 
     // console.log("lacjtags", bike._embedded["wp:term"][1]);
 
-    let colors = bike._embedded["wp:term"][1];
-    if (colors.length) {
-      //   alert("hey");
-      cloneEl.querySelector(".colour span").textContent = "";
-      const ulEl = document.createElement("ul");
-      colors.forEach((color) => {
-        const liEl = document.createElement("li");
-        liEl.style.backgroundColor = color.name;
-        ulEl.appendChild(liEl);
-      });
-      cloneEl.querySelector(".colour span").appendChild(ulEl);
-    }
+    // let colors = bike._embedded["wp:term"][1];
+    // if (colors.length) {
+    //   //   alert("hey");
+    //   cloneEl.querySelector(".colour span").textContent = "";
+    //   const ulEl = document.createElement("ul");
+    //   colors.forEach((color) => {
+    //     const liEl = document.createElement("li");
+    //     liEl.style.backgroundColor = color.name;
+    //     ulEl.appendChild(liEl);
+    //   });
+    //   cloneEl.querySelector(".colour span").appendChild(ulEl);
+    // }
 
     const parentEl = document.querySelector("main");
     parentEl.appendChild(cloneEl);
